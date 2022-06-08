aba1e2465a131cfd221dc2513815a4c5fa53bb40
Noah Weiss <93292627+noahweiss890@users.noreply.github.com>
2022-06-09 00:59:38 +0300

    Update test.c

--- test.c
+++ test.c
@@ -4,23 +4,21 @@
 int main() {
 
     mymkfs(10240);
-    int fd1 = myopen("noah", 0);
-    int fd2 = myopen("rashi", 0);
-    int fd3 = myopen("noah", 0);
+    // FD IS RETURNING -1, NEED TO CHECK WHY
+    int fd1 = myopen("/noah", 0);
+    int fd2 = myopen("/rashi", 0);
+    int fd3 = myopen("/noah", 0);
     char buff[] = "hello world!";
     mywrite(fd1, buff, 12);
     mylseek(fd1, -12, SEEK_CUR);
-    mymount("myFile.txt");
-    myprint();
+    // mymount("myFile.txt");
     char text[100];
     myread(fd1, text, 12);
     printf("read from fd1: :%s:\n", text);
     printf("fd1 is: %d\n", fd1);
     printf("fd2 is: %d\n", fd2);
     printf("fd3 is: %d\n", fd3);
-    // printf("sizeof sb: %ld\n", sizeof(superblock));
-    // printf("sizeof inode: %ld\n", sizeof(inode));
-    // printf("sizeof block: %ld\n", sizeof(block));
+    myprint();
     printf("done\n");
 
     return 0;
