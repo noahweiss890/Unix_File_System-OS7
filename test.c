3cdb7f46ee6b775697fcb535972107e79b172d86
Noah Weiss <93292627+noahweiss890@users.noreply.github.com>
2022-06-10 14:03:42 +0300

    Update test.c

--- test.c
+++ test.c
@@ -11,9 +11,9 @@ int main() {
     int fd1 = myopen("/noah", O_CREAT);
     int fd11 = myopen("/noah", 0);
     // myDIR* md1 = myopendir("/rashi");
-    // myDIR* md2 = myopendir("/rashi/bear");
-    myDIR* md3 = myopendir("/rashi/bear/weiss");
+    myDIR* md2 = myopendir("/rashi/bear");
     int fd5 = myopen("/rashi/bear/weiss/pachino", O_CREAT);
+    // myDIR* md3 = myopendir("/rashi/bear/bitchface/bla");
     char buff[] = "hello world!";
     int bla = 5;
     for(int i = 0; i < bla; i++) {
@@ -29,19 +29,20 @@ int main() {
     printf("fd1 is: %d\n", fd1);
     printf("fd11 is: %d\n", fd11);
     // printf("md1 is: %d\n", md1->inode);
-    // printf("md2 is: %d\n", md2->inode);
+    printf("md2 is: %d\n", md2->inode);
     // printf("md3 is: %d\n", md3->inode);
     printf("fd5 is: %d\n", fd5);
-    mydirent* mrent = myreaddir(md3); // THIS IS RETURNING NULL PROBABLY, NEED TO CHECK WHY
-    while(mrent != NULL) {
-        printf("obj name -> :%s:\n", mrent->d_name);
-        free(mrent);
-        mydirent* mrent = myreaddir(md3);
-    }
-    free(mrent);
+    // mydirent* mrent = myreaddir(md3);
+    // printf("mrent is -> %p\n", mrent);
+    // while(mrent != NULL) {
+    //     printf("obj name -> :%s:\n", mrent->d_name);
+    //     free(mrent);
+    //     mrent = myreaddir(md3);
+    // }
+    // free(mrent);
     myprint();
     // myclosedir(md1);
-    // myclosedir(md2);
+    myclosedir(md2);
     // myclosedir(md3);
     myclose(fd1);
     myclose(fd5);
