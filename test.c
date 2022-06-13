6774f77fdfdf43e87f93daf283a672ed4728b82a
Noah Weiss <93292627+noahweiss890@users.noreply.github.com>
2022-06-13 21:30:26 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,76 +1,48 @@
 #include <stdio.h>
-#include "my_ufs.h"
+#include <assert.h>
 #include "mystdio.h"
 
 int main() {
-    
+
     mymkfs(10240);
-    // // mymount("myFile.txt", " ", " ", 0, " "); // SEG FAULT IN THIS FUNCTION
-    // // printf("mounted\n");
-    // // return 0;
-    // int fd1 = myfopen("/noah", O_CREAT);
-    // int fd11 = myopen("/noah", 0);
-    // // myDIR* md1 = myopendir("/rashi");
-    // myDIR* md2 = myopendir("/rashi/bear");
-    // int fd5 = myopen("/rashi/bear/weiss/pachino", O_CREAT);
-    // // myDIR* md3 = myopendir("/rashi/bear/bitchface/bla");
-    // char buff[] = "hello world!";
-    // int bla = 5;
-    // for(int i = 0; i < bla; i++) {
-    //     // printf("write # %d\n", i);
-    //     mywrite(fd5, buff, 12);
-    // }
-    // // mywrite(fd5, buff, 12);
-    // int curzz = mylseek(fd5, -12, SEEK_CUR);
-    // // printf("curzz returned: %d\n", curzz);
-    // char text[1000] = {'\0'};
-    // myread(fd5, text, 12);
-    // printf("read from fd5: :%s:\n", text);
-    // printf("fd1 is: %d\n", fd1);
-    // printf("fd11 is: %d\n", fd11);
-    // // printf("md1 is: %d\n", md1->inode);
-    // printf("md2 is: %d\n", md2->inode);
-    // // printf("md3 is: %d\n", md3->inode);
-    // printf("fd5 is: %d\n", fd5);
-    // // mydirent* mrent = myreaddir(md3);
-    // // printf("mrent is -> %p\n", mrent);
-    // // while(mrent != NULL) {
-    // //     printf("obj name -> :%s:\n", mrent->d_name);
-    // //     free(mrent);
-    // //     mrent = myreaddir(md3);
-    // // }
-    // // free(mrent);
-    // myprint();
-    // // myclosedir(md1);
-    // myclosedir(md2);
-    // // myclosedir(md3);
-    // myclose(fd1);
-    // myclose(fd5);
-    // myclose(fd11);
-    // free_mem();
-    // printf("done\n");
+    // mymount("myFile.txt", " ", " ", 0, " "); // SEG FAULT IN THIS FUNCTION
+    // printf("mounted\n");
 
-    myFILE* fp1 = myfopen("/noah", "w");
-    char bla[] = "hello rashi! and noah!";
-    int wrote = myfwrite(bla, 22, 1, fp1);
-    printf("wrote-> %d\n", wrote);
-
-    myFILE* fp2 = myfopen("/noah", "r");
-    char res[13] = {'\0'};
-    int read = myfread(res, 12, 1, fp2);
-    printf("read-> %d\n", read);
-    printf("please-> :%s:\n", res);
-    myfseek(fp2, -12, SEEK_CUR);
-    char res1[23] = {'\0'};
-    read = myfread(res1, 22, 1, fp2);
-    printf("read-> %d\n", read);
-
-
-    printf("please-> :%s:\n", res1);
-    printf("done\n");
-
+    myFILE* fp1 = myfopen("/zion", "w"); // this will create a file "/zion"
     myfclose(fp1);
+
+    myFILE* fp2 = myfopen("/zion", "r+"); // open in read and write mode
+    char bla[] = "hello rashi! and noah!";
+
+    int wrote = myfprintf(fp2, "%c%c%c%c%c", *bla, *(bla+1), *(bla+2), *(bla+3), *(bla+4));
+    assert(wrote == 5);
+    printf("successfully wrote in myfprintf -> %d\n", wrote);
+
+    wrote = myfwrite(bla + 5, 17, 1, fp2);
+    assert(wrote == 17);
+    printf("successfully wrote in myfwrite -> %d\n", wrote);
+
+    int seeked = myfseek(fp2, 0, SEEK_SET);
+    assert(seeked == 0);
+
+    char res[23] = {'\0'};
+    int read = myfscanf(fp2, "%c %c %c %c %c", res, res+1, res+2, res+3, res+4);
+    assert(read == 5);
+    printf("successfully read in myfscanf -> %d\n", read);
+    printf("read from file -> %c%c%c%c%c\n", *res, *(res+1), *(res+2), *(res+3), *(res+4));
+    
+    seeked = myfseek(fp2, 0, SEEK_SET);
+    assert(seeked == 0);
+    
+    read = myfread(res, 22, 1, fp2);
+    assert(read == 22);
+    printf("successfully read in myfread -> %d\n", read);
+    printf("read from file -> %s\n", res);
+    assert(strcmp(res, "hello rashi! and noah!") == 0);
+    
     myfclose(fp2);
+
+    printf("done\n");
 
     return 0;
 }
