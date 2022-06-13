1962667627190dc65e55067ef2dde0fad0cce272
Noah Weiss <93292627+noahweiss890@users.noreply.github.com>
2022-06-14 00:35:02 +0300

    Update my_ufs.c

--- my_ufs.c
+++ my_ufs.c
@@ -7,15 +7,13 @@ myopenfile fds[MAX_FILES];
 int avail_fd = 3;
 
 void mymkfs(int s) {
+    if(s <= 0) {
+        perror("s must be positive");
+        return;
+    }
     sb.inodes_amount = (s / sizeof(inode)) / 10;
     sb.blocks_amount = (s - (sb.inodes_amount * sizeof(inode)) - sizeof(superblock)) / sizeof(block);
     sb.block_size = sizeof(block);
-
-    // printf("inodes: %d\n", sb.inodes_amount);
-    // printf("blocks: %d\n", sb.blocks_amount);
-    // printf("size of superblock: %ld\n", sizeof(superblock));
-    // printf("size of inode: %ld\n", sizeof(inode));
-    // printf("size of block: %ld\n", sizeof(block));
 
     inodes = (inode*)malloc(sizeof(inode) * sb.inodes_amount);
     if(inodes == NULL) {
@@ -41,52 +39,64 @@ void mymkfs(int s) {
     }
     strcpy(sb.root.d_name, "/");
     sb.root.inode_num = allocate_file_folder(1);
-    save_to_file();
+    save_to_file("myFile.txt");
 }
 
 void free_mem() {
-    free(inodes);
-    free(blocks);
-    printf("freed\n");
+    if(inodes != NULL && blocks != NULL) {
+        free(inodes);
+        free(blocks);
+        printf("freed\n");
+    }
+    else {
+        perror("error");
+    }
 }
 
 int mymount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data) {
-    printf("size of superblock: %ld\n", sizeof(superblock));
-    printf("size of inode: %ld\n", sizeof(inode));
-    printf("size of block: %ld\n", sizeof(block));
-
-    FILE* fp;
-    fp = fopen(source, "r");
-    if(fp == NULL) {
-        perror("fopen");
+    if(inodes != NULL && blocks != NULL) {
+        free_mem();
+    }
+    if (source == NULL && target == NULL) {
+        perror("error");
         return -1;
     }
-    fread(&sb, sizeof(superblock), 1, fp);
-    printf("inodes: %d\n", sb.inodes_amount);
-    printf("blocks: %d\n", sb.blocks_amount);
-    inodes = (inode*)malloc(sizeof(inode) * sb.inodes_amount);
-    if(inodes == NULL) {
-        perror("no memory");
-        exit(0);
-    }
-    blocks = (block*)malloc(sizeof(block) * sb.blocks_amount);
-    if(blocks == NULL) {
-        perror("no memory");
-        exit(0);
-    }
-    fread(&inodes, sizeof(inode), sb.inodes_amount, fp);
-    fread(&blocks, sizeof(block), sb.blocks_amount, fp);    
-    fclose(fp);
-    // printf("sb info -> inodes amount: %d blocks amount: %d\n", sb.inodes_amount, sb.blocks_amount);
+    if (source != NULL) {
+        FILE* fp;
+        fp = fopen(source, "r");
+        if(fp == NULL) {
+            perror("fopen");
+            return -1;
+        }
+        fread(&sb, sizeof(superblock), 1, fp);
+        inodes = (inode*)malloc(sizeof(inode) * sb.inodes_amount);
+        if(inodes == NULL) {
+            perror("no memory");
+            exit(0);
+        }
+        blocks = (block*)malloc(sizeof(block) * sb.blocks_amount);
+        if(blocks == NULL) {
+            perror("no memory");
+            exit(0);
+        }
+        fread(inodes, sizeof(inode), sb.inodes_amount, fp);
+        fread(blocks, sizeof(block), sb.blocks_amount, fp);    
     
-    printf("bla: %d\n", inodes[0].type); // THIS ISNT WORKING CUZ SEGMENTATION FAULT
-    
+        fclose(fp);
+    }
+    if(target != NULL) {
+        save_to_file(target);
+    }
     return 0;
 }
 
-void save_to_file() {
+void save_to_file(const char *pathname) {
+    if(inodes == NULL || blocks == NULL) {
+        perror("error");
+        return;
+    }
     FILE* fp;
-    fp = fopen("myFile.txt", "w+");
+    fp = fopen(pathname, "w");
     if(fp == NULL) {
         perror("fopen");
         return;
@@ -98,6 +108,10 @@ void save_to_file() {
 }
 
 void myprint() {
+    if(inodes == NULL || blocks == NULL) {
+        perror("error");
+        return;
+    }
     printf("superblock info:\n");
     printf("\tinodes amount: %d\n", sb.inodes_amount);
     printf("\tblocks amount: %d\n", sb.blocks_amount);
@@ -114,6 +128,10 @@ void myprint() {
 }
 
 int find_available_inode() {
+    if(inodes == NULL) {
+        perror("error");
+        return -1;
+    }
     for(int i = 0; i < sb.inodes_amount; i++) {
         if(inodes[i].first_block == -1) {
             return i;
@@ -123,6 +141,10 @@ int find_available_inode() {
 }
 
 int find_available_block() {
+    if(blocks == NULL) {
+        perror("error");
+        return -1;
+    }
     for(int i = 0; i < sb.blocks_amount; i++) {
         if(blocks[i].available == 1) {
             return i;
@@ -132,6 +154,10 @@ int find_available_block() {
 }
 
 int allocate_file_folder(int type) {
+    if(inodes == NULL || blocks == NULL) {
+        perror("error");
+        return -1;
+    }
     int avail_inode = find_available_inode();
     if(avail_inode == -1) {
         return -1;
@@ -150,6 +176,10 @@ int allocate_file_folder(int type) {
 }
 
 int myopen(const char *pathname, int flags) {
+    if(inodes == NULL || blocks == NULL || pathname == NULL) {
+        perror("error");
+        return -1;
+    }
     char path[NAME_SIZE] = {'\0'};
     char* last_slash = strrchr(pathname, '/');
     strncpy(path, pathname, last_slash - pathname);
@@ -161,7 +191,7 @@ int myopen(const char *pathname, int flags) {
     while(mrent != NULL) {
         if(strcmp(mrent->d_name, last_slash + 1) == 0) {
             fds[avail_fd].my_inode = mrent->inode_num;
-            if(flags & (O_CREAT | O_WRONLY)) {
+            if(flags & O_APPEND) {
                 fds[avail_fd].curser = inodes[mrent->inode_num].length;
             }
             else {
@@ -176,12 +206,7 @@ int myopen(const char *pathname, int flags) {
         int new_inode = allocate_file_folder(0);
         attach_to_folder(mdir, new_inode, last_slash + 1);
         fds[avail_fd].my_inode = new_inode;
-        if(flags & O_APPEND) {
-            fds[avail_fd].curser = inodes[mrent->inode_num].length;
-        }
-        else {
-            fds[avail_fd].curser = 0;
-        }
+        fds[avail_fd].curser = 0;
         myclosedir(mdir);
         return avail_fd++;
     }
@@ -191,7 +216,11 @@ int myopen(const char *pathname, int flags) {
 }
 
 int myclose(int myfd) {
-    if(myfd >= avail_fd) {
+    if(inodes == NULL || blocks == NULL) {
+        perror("error");
+        return -1;
+    }
+    if(myfd >= avail_fd || fds[myfd].my_inode == -1) {
         return -1;
     }
     fds[myfd].my_inode = -1;
@@ -199,7 +228,8 @@ int myclose(int myfd) {
 }
 
 ssize_t myread(int myfd, void *buf, size_t count) {
-    if(fds[myfd].my_inode == -1) {
+    if(inodes == NULL || blocks == NULL || fds[myfd].my_inode == -1) {
+        perror("error");
         return -1;
     }
     char* text = (char*)buf;
@@ -226,7 +256,8 @@ ssize_t myread(int myfd, void *buf, size_t count) {
 }
 
 ssize_t mywrite(int myfd, const void *buf, size_t count) {
-    if(fds[myfd].my_inode == -1) {
+    if(inodes == NULL || blocks == NULL || fds[myfd].my_inode == -1) {
+        perror("error");
         return -1;
     }
     char* text = (char*)buf;
@@ -262,11 +293,15 @@ ssize_t mywrite(int myfd, const void *buf, size_t count) {
         fds[myfd].curser++;
         inodes[fds[myfd].my_inode].length++;
     }
-    save_to_file();
+    save_to_file("myFile.txt");
     return count;
 }
 
 off_t mylseek(int myfd, off_t offset, int whence) {
+    if(inodes == NULL || blocks == NULL) {
+        perror("error");
+        return -1;
+    }
     if(whence == SEEK_SET) {
         fds[myfd].curser = offset;
         return fds[myfd].curser;
@@ -282,7 +317,11 @@ off_t mylseek(int myfd, off_t offset, int whence) {
     return -1;
 }
 
-void attach_to_folder(myDIR* mdir, int ino, const char* filename) {    
+void attach_to_folder(myDIR* mdir, int ino, const char* filename) {
+    if(inodes == NULL || blocks == NULL || mdir == NULL || filename == NULL) {
+        perror("error");
+        return;
+    }
     mydirent md;
     strcpy(md.d_name, filename);
     md.inode_num = ino;
@@ -323,10 +362,14 @@ void attach_to_folder(myDIR* mdir, int ino, const char* filename) {
         curser++;
     }
     inodes[mdir->inode].mydirent_amount++;
-    save_to_file();
+    save_to_file("myFile.txt");
 }
 
 myDIR* myopendir(const char *name) {
+    if(inodes == NULL || blocks == NULL || name == NULL) {
+        perror("error");
+        return NULL;
+    }
     if(strcmp(name, "\0") == 0) {
         myDIR* md = (myDIR*)malloc(sizeof(myDIR));
         if(md == NULL) {
@@ -364,6 +407,10 @@ myDIR* myopendir(const char *name) {
 }
 
 struct mydirent* myreaddir(myDIR *dirp) {
+    if(inodes == NULL || blocks == NULL || dirp == NULL) {
+        perror("error");
+        return NULL;
+    }
     if(dirp->curser / sizeof(mydirent) >=  inodes[dirp->inode].mydirent_amount) {
         return NULL;
     }
@@ -391,7 +438,11 @@ struct mydirent* myreaddir(myDIR *dirp) {
     return (struct mydirent*)mrent;
 }
 
-int myclosedir(myDIR *dirp) { // NOT DONE
+int myclosedir(myDIR *dirp) {
+    if(dirp == NULL) {
+        perror("error");
+        return -1;
+    }
     free(dirp);
     return 0;
 }
